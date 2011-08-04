89dd1fba2d41b64e3807b1f34e0e55e1d7566531
Bas van Driel <darkslayerzz@hotmail.com>
2011-08-04 17:30:15 +0200

    whoops.. well this  works better i guess :p

--- Play.bat
+++ Play.bat
@@ -1,5 +1,6 @@
 @echo off
 title Project Benelux - 317 - Client
 cd ./Class/
-"C:\Program Files\Java\jre6\bin\java.exe" -Xmx512m Gui 30 0 highmem members 32
-pause.
+java -Xmx512m Gui 30 0 highmem members 32
+pause
+
